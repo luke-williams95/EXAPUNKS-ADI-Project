7e86b09b50ae814cd5b7e45caf54621c5c8a5580
Luke Williams <luke.williams@analog.com>
2022-02-04 15:27:20 -0800

    ADDI function to be tested. Probably broken.

--- EXA.py
+++ EXA.py
@@ -6,16 +6,16 @@ class EXA:
         # dict/map of all the known commands. 
         # Keys are the commands (str), the value pairs are a list that indicate each valid entry for an arugment
         self.functions = {
-                    'COPY': ('r/n', 'r')
-                    'ADDI': ('r/n', 'r/n', 'r')
-                    'SUBI': ('r/n', 'r/n', 'r')
-                    'MULI': ('r/n', 'r/n', 'r')
-                    'DIVI': ('r/n', 'r/n', 'r')
-                    'TEST': ('r/n', 'op', 'r/n')
-                    'MARK': 
-                    'JUMP': 
-                    'TJMP': 
-                    'FJMP': 
+                    'COPY': (self.COPY, ['r/n', 'r'])
+                    'ADDI': (self.ADDI, ['r/n', 'r/n', 'r'])
+                    'SUBI': (self.SUBI, ['r/n', 'r/n', 'r'])
+                    'MULI': (self.MULI, ['r/n', 'r/n', 'r'])
+                    'DIVI': (self.DIVI, ['r/n', 'r/n', 'r'])
+                    'TEST': (self.TEST, ['r/n', 'op', 'r/n'])
+                    'MARK': (self.MARK, [])
+                    'JUMP': (self.JUMP, [])
+                    'TJMP': (self.TJMP, [])
+                    'FJMP': (self.FJMP, [])
                 }
                     
         
@@ -24,11 +24,11 @@ class EXA:
         cmd_tuple = EXA.parse(usr_in) # EXA.parsed 'ADDI arg0 arg1 arg2' --> ('ADDI', [arg0, arg1, arg2])
         if not self.check_input(cmd_tuple) # checks for valid argument entries
             print('Invalid input!')
-            return
-        return self.
+            return self.function[
+        self.functions[cmd_tuple[0]][0](cmd_tuple[1]) # this calls the appropriate command method and gives the arguments.
         
     @staticmethod
-    def parse(usr_in): # This works within the EXA class as long as there is no 'self' argument. Ask Dave about where to put this
+    def parse(usr_in): # Add a function call to a valid arg to check args in this.
             """Digests the user input to a useful format."""
             # "   ADDI   30  X T" ===> ("ADDI", ["30", "X", "T"])
             string_list = usr_in.strip().split(' ', 1) # Split the first string from usr_in using " " as your delimiter
@@ -42,7 +42,9 @@ class EXA:
     def check_input(self, cmd_tuple):
         """Accepts the cmd_tuple and outputs a list of tuples, 
         each tuple showing a valid argument entry and the given argument."""
-        valid_args = self.functions(cmd_tuple[0]) # valid_args = ['r/n', 'r/n', 'r']
+        return True # pass this for now
+        
+        valid_args = self.functions(cmd_tuple[0])[1] # valid_args = ['r/n', 'r/n', 'r']
         given_args = cmd_tuple[1] # given_args = ['30', 'X', 'T']
         
         if len(valid_args) != len(given_args): # check first that there are the correct number of arguments.
@@ -51,9 +53,13 @@ class EXA:
         
         args_list = [(valid, given) for valid, given in zip(valid_args, given_args)] # make tuples of arg by arg comparison valid vs. given
         
-        self.valid_args(args_list) # args_list = [('r/n', '30'),..., ('r', 'T')]
+        self.valid_input(args_list) # args_list = [('r/n', '30'),..., ('r', 'T')]
     
-    def valid_args(args_list):
+    def isregister(arg):
+        """Returns True if argument is a valid register."""
+        pass
+        
+    def valid_input(args_list):
         """checks each entry for valid argument."""
         for tup in args_list: # tup = ('r/n', 'X'), ('op', '='), etc.
         
@@ -82,27 +88,25 @@ class EXA:
             return int(operand)
     
     def ADDI(self, cmd_list):
-        return self.registers[
-        
-        
-        
         """Add the register values or numbers and store result in register. Called from interpret()"""
         # At this point we expect cmd_list to be a properly formatted list object
         # inspects the command list and puts the proper values in the operand_list
         # Should crash if len(cmd_list) != 3
-        operand_list = [[cmd] for cmd in cmd_list
-                                  self.inspect(cmd)]
-        
-        self.registers[operand_list[2]] = operand_list[0] + operand_list[1]
-         
+        # ['X', 50, 'X']
+        # X = 20
+        operand_list = [inspect(cmd_list[0]), inspect(cmd_list[1])]
+        self.registers[cmd_list[2]] = operand_list[0] + operand_list[1] # update register value
+        print(self.registers[cmd_list[2]]) # check that register value was updated
         return operand_list[0] + operand_list[1]
 
         
         
 
 if __name__ == '__main__': # For local testing
-    op_tuple = EXA.parse("   ADDI  30  X     T")
-    #print(op_tuple)
+    exa = EXA()
+    string = 'ADDI 5 5 X'
+    exa.interpret(string)
+    print(exa.registers['X'])
 
     #exa = EXA()
     #exa.registers['X'] = 2
